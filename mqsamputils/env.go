e3de9c9a93bcf8afc41cee080df9dfff8bf90c83
Nilo Vila <eb_nvilaz@bancoripley.com>
2023-02-16 21:34:48 +0100

    add modification

--- mqsamputils/env.go
+++ mqsamputils/env.go
@@ -51,7 +51,7 @@ var MQ_ENDPOINTS MQEndpoints
 
 const FULL_STRING = -1
 
-func init(num int64) {
+func init() {
 	jsonFile, err := os.Open("/usr/local/go/src/mq-ibm-golang/mqsamputils/env.json")
 	defer jsonFile.Close()
 
